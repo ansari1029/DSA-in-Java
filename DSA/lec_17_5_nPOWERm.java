6cc5cb6b873739e21a4cd46da37dffba2d57377d
Ansari Mohd Asad <ansarimohdasad308@gmail.com>
2026-09-22 23:07:18 +0530

    did other program for recursion

---
+++ DSA/lec_17_5_nPOWERm.java
@@ -0,0 +1,21 @@
+package DSA;
+
+public class lec_17_5_nPOWERm {
+	public static int calPower(int n, int m) {
+		if(n==0)
+			return 0;
+		if(m==0)
+			return 1;
+		int nPowm_m1 = calPower(n, m-1);
+		int nPower = n * nPowm_m1;
+		return nPower;
+	}
+
+	public static void main(String[] args) {
+		int n=2, m=3;
+		int ans=calPower(n, m);
+		System.out.println("The Answer of "+n+" to the Power "+m+" is :"+ans);
+
+	}
+
+}
